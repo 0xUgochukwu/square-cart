77ab06dcd144d76c6fdb8beaf2e8ce3572570833
Dev Bash <ikorosamuel1@gmail.com>
2024-04-17 00:56:17 +0100

    feat: send tiktok user info to socket notification

--- packages/api/src/services/socket-transactions.js
+++ packages/api/src/services/socket-transactions.js
@@ -5,27 +5,27 @@ const socketIo = require("socket.io");
 let io;
 
 function initSocket(server) {
-  io = socketIo(server, {
-    cors: {
-      origin: "*",
-      methods: ["GET", "POST"],
-    },
-  });
-
-  io.on("connection", (socket) => {
-    console.log("A client connected");
-
-    socket.on("disconnect", () => {
-      console.log("A client disconnected");
+    io = socketIo(server, {
+        cors: {
+            origin: "*",
+            methods: ["GET", "POST"],
+        },
     });
-  });
+
+    io.on("connection", (socket) => {
+        console.log("A client connected");
+
+        socket.on("disconnect", () => {
+            console.log("A client disconnected");
+        });
+    });
 }
 
 function getIo() {
-  if (!io) {
-    throw new Error("Socket.io not initialized!");
-  }
-  return io;
+    if (!io) {
+        throw new Error("Socket.io not initialized!");
+    }
+    return io;
 }
 
 module.exports = { initSocket, getIo };
--- packages/extension/src/tiktok/script.js
+++ packages/extension/src/tiktok/script.js
@@ -2,6 +2,7 @@ const iframe = document.createElement("iframe");
 iframe.id = "frame";
 const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get("username");
+const user = urlParams.get("user");
 
-iframe.src = `http://localhost:5173/social/tiktok/${username}`;
+iframe.src = `http://localhost:5173/social/tiktok/${username}?user=${user}`;
 document.body.appendChild(iframe);
