6e149940dbb006704a031c4d39ccf9c19fb630da
Dev Bash <ikorosamuel1@gmail.com>
2024-04-29 23:23:05 +0100

    Update

--- packages/extension/src/tiktok/script.js
+++ packages/extension/src/tiktok/script.js
@@ -4,5 +4,6 @@ const urlParams = new URLSearchParams(window.location.search);
 const username = urlParams.get("username");
 const user = urlParams.get("user");
 
-iframe.src = `http://localhost:5173/social/tiktok/${username}?user=${user}`;
+// iframe.src = `http://localhost:5173/social/tiktok/${username}?user=${user}`;
+iframe.src = `https://squarecart-client.netlify.app/social/tiktok/${username}?user=${user}`;
 document.body.appendChild(iframe);
