b077019eef36bd580bea8b0a7ee443b8a8123c6d
chionye <michael.chionye@gmail.com>
2024-04-29 21:18:44 +0100

    update: change label for social on register

--- packages/frontend/src/pages/auth/Register.tsx
+++ packages/frontend/src/pages/auth/Register.tsx
@@ -95,7 +95,7 @@ const Register = () => {
           />
         </div>
         <div className='grid w-full max-w-sm items-center gap-1.5 mt-5'>
-          <Label htmlFor='email'>Social</Label>
+          <Label htmlFor='social'>Tiktok Username</Label>
           <Input
             type='text'
             className='w-full'
